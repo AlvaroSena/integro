5e655716b4780e5b7d0f2147557eca4c5da970c2
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-08-19 18:41:42 -0300

    chore: react + ts + tailwindcss setup

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,10 +1,21 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import { errorHandler } from "./infra/middlewares/error-handler";
 import { routes } from "./infra/routes";
 
-const app = express();
+const app: Application = express();
+app.use(
+  cors({
+    origin: process.env.WEB_ORIGIN!,
+    methods: "*",
+  })
+);
 app.use(express.json());
 app.use("/v1", routes);
+
+app.get("/", (request: Request, reply: Response) => {
+  return reply.json({ message: "Hello from Integro" });
+});
 
 app.use(errorHandler);
 
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
